7570d4e1ea89993b6909b6d6e76163982a1c2246
ravimohansri@hotmail.com <ravimohansri@hotmail.com>
2019-06-08 21:07:53 -0700

    Initial

--- ConsoleApp4WebJob/Program.cs
+++ ConsoleApp4WebJob/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Text;
+using System.Net;
+using RestSharp;
 
 namespace ConsoleApp4WebJob
 {
@@ -10,6 +12,14 @@ namespace ConsoleApp4WebJob
     {
         static void Main(string[] args)
         {
+            string url = "https://icanhazdadjoke.com";
+            var client = new RestClient(url);
+            var request = new RestRequest("/", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+            //var joke = JsonConvert.DeserializeObject<DadJoke>(response.Content);
+            var joke = response.Content;
+            Console.WriteLine(joke);
         }
     }
 }
